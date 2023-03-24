88058fa0f19ed7615e34d7e8fd245b4190da9db3
ismaelcoulibaly <@ismael>
2023-03-23 23:17:34 -0400

    swagger config

--- src/main/java/com/ondza/essey/controllers/ReservationController.java
+++ src/main/java/com/ondza/essey/controllers/ReservationController.java
@@ -4,6 +4,10 @@ package com.ondza.essey.controllers;
 import com.ondza.essey.entities.Reservation;
 import com.ondza.essey.entities.ResponseModel;
 import com.ondza.essey.services.ReservationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/reservations")
+@Api(value = "Backend oNDZA ESSEY")
 public class ReservationController {
 
     private ReservationService reservationService;
@@ -20,6 +25,12 @@ public class ReservationController {
     public ReservationController(ReservationService reservationService){
         this.reservationService = reservationService;
     }
+
+    @ApiOperation(value = "Get a greeting", response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved greeting"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
+    })
     @PostMapping("/new")
     public ResponseEntity<ResponseModel> createReservation(@RequestBody Reservation reservation) {
 
