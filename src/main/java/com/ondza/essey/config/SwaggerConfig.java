28b9f4c9c6e37ae862861b8f1a2612649b4216d1
ismaelcoulibaly <@ismael>
2023-04-13 11:54:18 +0200

    application.properties config complete

--- src/main/java/com/ondza/essey/config/SwaggerConfig.java
+++ src/main/java/com/ondza/essey/config/SwaggerConfig.java
@@ -14,13 +14,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ondza.essey.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.ondza.essey"))
                 .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/");
+                .build();
     }
 
     private ApiInfo apiInfo() {
