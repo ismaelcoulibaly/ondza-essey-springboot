92ff08d2cd6ccdc7f17a5e778c4c4d3578073096
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-06-17 13:41:57 -0400

    Swagger works

--- src/main/java/com/ondza/essey/OndzaEsseyBackendApplication.java
+++ src/main/java/com/ondza/essey/OndzaEsseyBackendApplication.java
@@ -3,12 +3,19 @@ package com.ondza.essey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class OndzaEsseyBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OndzaEsseyBackendApplication.class, args);
     }
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.ondza.essey.controllers"))
+//                .build();
+//    }
 
 }
