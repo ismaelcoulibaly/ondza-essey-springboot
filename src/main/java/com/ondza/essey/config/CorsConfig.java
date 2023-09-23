df729454b9065752c70123ea31df11edcc30c92e
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-09-23 17:36:01 -0400

    changed crossOrigin to accept requests from anywhere

--- src/main/java/com/ondza/essey/config/CorsConfig.java
+++ src/main/java/com/ondza/essey/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Replace with your frontend's origin(s)
+                .allowedOrigins("https://ondza-essey-backend-production.up.railway.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
