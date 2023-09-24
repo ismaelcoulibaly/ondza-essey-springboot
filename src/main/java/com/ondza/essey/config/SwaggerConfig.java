884d5dc4cec2c28d1e013ea86f76df09837985d9
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-09-23 22:49:29 -0400

    Fix SwaggerConfig to send request from Swagger UI

--- src/main/java/com/ondza/essey/config/SwaggerConfig.java
+++ src/main/java/com/ondza/essey/config/SwaggerConfig.java
@@ -2,13 +2,13 @@ package com.ondza.essey.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -21,14 +21,6 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.ondza.essey.controllers"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Ondza Essey")
-                .description("Your API Description")
-                .version("1.0.0")
-                .build();
+                .protocols(Collections.singleton("https")); // Set the protocol to HTTPS
     }
 }
